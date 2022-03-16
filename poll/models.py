439fb4ad7cf1270ba740d46ca6707a28d8b1e99a
v-rishi <rishi@yodaplus.com>
2022-03-16 17:13:57 +0530

    Commit after creating the models and simple HttpResponse as views

--- poll/models.py
+++ poll/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+"""
+This app will have 2 models - a Question model and a 
+Choices model. The Choices model will reference the Question model.
+"""
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=300)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choices(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
--- poll/views.py
+++ poll/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+# import datetime
 # Create your views here.
+
+def index(request):
+    # now = datetime.datetime.now()
+    # local = now.astimezone()
+    return HttpResponse("Welcome to the Home page of the polling app")
+
+
+def details(request, question_id):
+    return HttpResponse("You're looking at question %s" %question_id)
+
+
+def results(request, question_id):
+    return HttpResponse("You're looking at results of  %s" %question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're looking at votes of question %s" %question_id)
+
